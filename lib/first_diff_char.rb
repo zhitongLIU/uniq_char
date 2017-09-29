a3be53722529b838966bb06368a5a586c3eb1e14
zhitongLIU <zhitonggm.liu@gmail.com>
2017-09-29 12:59:39 +0800

    Add :first_diff_char and :first_diff_char2 method

--- lib/first_diff_char.rb
+++ lib/first_diff_char.rb
@@ -1,1 +1,20 @@
 require 'first_diff_char/identity'
+
+#
+class String
+  def first_uniq_char
+    # easiest implementation to get the first char which has only one count
+    # O(n^2) worst case, best 1
+    each_char.find { |c| count(c) == 1 }
+  end
+
+  def first_uniq_char2
+    # liner implementation
+    # O(2n) worst case, best n + 1
+    nb_count = {}
+    chars.each { |c| nb_count[c] = nb_count[c].nil? ? 1 : nb_count[c] + 1 }
+    couple = nb_count.find { |_k, v| v == 1 }
+    # return the key of the key value couple(the first one in the couple)
+    couple.nil? ? nil : couple.first
+  end
+end
--- spec/lib/first_diff_char/first_diff_char_spec.rb
+++ spec/lib/first_diff_char/first_diff_char_spec.rb
@@ -1,5 +1,45 @@
 require 'spec_helper'
 
+# rubocop: disable Metrics/BlockLength
 describe 'FirstDiffChar' do
-  it 'should be tested'
+  context ':first_uniq_char' do
+    it 'should respond to :first_uniq_char for a String object' do
+      expect(String.instance_methods.include?(:first_uniq_char)).to be_truthy
+    end
+
+    it 'should return the first uniq character in a string' do
+      x = 'aabbsc'
+      expect(x.count('s')).to eq(1)
+      expect(x.count('c')).to eq(1)
+      expect(x.index('s') < x.index('c')).to be true
+
+      expect(x.first_uniq_char).to eq('s')
+    end
+
+    it 'should return nil if string does not contain uniq char' do
+      x = 'aabb'
+      expect(x.first_uniq_char).to be_nil
+    end
+  end
+
+  context ':first_uniq_char2' do
+    it 'should respond to :first_uniq_char2 for a String object' do
+      expect(String.instance_methods.include?(:first_uniq_char2)).to be_truthy
+    end
+
+    it 'should return the first uniq character in a string' do
+      x = 'aabbsc'
+      expect(x.count('s')).to eq(1)
+      expect(x.count('c')).to eq(1)
+      expect(x.index('s') < x.index('c')).to be true
+
+      expect(x.first_uniq_char2).to eq('s')
+    end
+
+    it 'should return nil if string does not contain uniq char' do
+      x = 'aabb'
+      expect(x.first_uniq_char2).to be_nil
+    end
+  end
 end
+# rubocop: enable Metrics/BlockLength
